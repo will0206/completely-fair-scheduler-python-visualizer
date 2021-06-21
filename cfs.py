afdbaf2b9892cde76cfba9407f9a302790ccc822
Ted Lin <vul3865@gmail.com>
2021-06-21 15:19:07 +0800

    [revise] for final upload

--- cfs.py
+++ cfs.py
@@ -28,8 +28,7 @@ def cfsSchedule(tasks: List[dict], quantum: int, logger: object):
     for i in range(0,len(tasks)):
         xcood.append([0.0])
         processes.append([0.0])
-
-    # its return can be call(callable), if it return false , it means it will can't not be call absolutely.
+    #Init process execute time data ,use for generate image
 
     get_vruntime: Callable[[dict], int] = lambda task: task["vruntime"]
     get_nice: Callable[[dict], int] = lambda task: task["nice"]
@@ -47,13 +46,11 @@ def cfsSchedule(tasks: List[dict], quantum: int, logger: object):
         processRuntime[str(t["pid"])] = 0
         listname = listname + ',' + 'process' + str(t["pid"])
     
-    print(processRuntime)
-    logger.debug(f'{listname}')
+    # logger.debug(f'{listname}')
     
     while (num := len(tasks_sorted)) > 0:
         total_weight_iter = 0
         vruntime = 0
-        # Add tasks that have arrived after previous iteration
         #從最後面(抵達時間最晚)開始加到tasks_sorted內
         for task in tasks[end:]:
             # 如果最先抵達的任務的時間 小於目前的時間軸時間 代表此任務已抵達
@@ -69,14 +66,10 @@ def cfsSchedule(tasks: List[dict], quantum: int, logger: object):
         for task_this_iteration in tasks_sorted:
             total_weight_iter += task_this_iteration["weight"]
 
-
-
-        # timeslice = floor(quantum / num)  # Dynamic timeslice
         timeslice = quantum #6ms
         min_task = tasks_sorted[0] #最早抵達的那個
 
-        # Time remaining for smallest task
-        t_rem = min_task["burst_time"] - min_task["exec_time"]
+        t_rem = min_task["burst_time"] - min_task["exec_time"]         # Time remaining for smallest task
 
         #min_granularity 0.75 ms
         if t_rem <= 0.75:
@@ -99,8 +92,8 @@ def cfsSchedule(tasks: List[dict], quantum: int, logger: object):
 
         # logger.debug(('execute time of tasks:',str(time),'t_rem:',str(t_rem),
         # 'vruntime of the task:',str(min_vruntime)))
-        # min_vruntime = get_vruntime(min_task) #一開始是0
-        min_nice = get_nice(min_task) 
+
+
 
         # display_tasks(tasks_sorted)
         # logger.debug(f'Executing task {min_task["pid"]} for {time} ms\n')
@@ -113,7 +106,7 @@ def cfsSchedule(tasks: List[dict], quantum: int, logger: object):
         min_task["turnaround_time"] += time
         timer += time #更新現在時間 
 
-        # Increment waiting and turnaround time of all other processes
+        # update waiting and turnaround time of all other processes
         for i in range(1, num):
             tasks_sorted[i]["waiting_time"] += time
             tasks_sorted[i]["turnaround_time"] += time
@@ -135,7 +128,7 @@ def cfsSchedule(tasks: List[dict], quantum: int, logger: object):
         processRuntime[nowExecutePid] = processRuntime[nowExecutePid] + time
 
         
-        printlog(tasks,min_task,timer,processRuntime,logger)
+        # printlog(tasks,min_task,timer,processRuntime,logger)
         taskIndex = tasks.index(min_task) #0 1 2
         
         for i in range(0,len(processes)):
@@ -148,58 +141,6 @@ def cfsSchedule(tasks: List[dict], quantum: int, logger: object):
 
     drawResult(xcood,processes)    
 
-
-def displayTasks(tasks: List[dict]):
-    headers = [
-        "ID",
-        "Arrival Time",
-        "Burst Time",
-        "Nice",
-        "Waiting Time",
-        "Turnaround Time",
-    ]
-    tasks_mat = []
-
-    for task in tasks:
-        tasks_mat.append(
-            [
-                task["pid"],
-                f"{task['arrival_time'] / 1000}",
-                f"{task['burst_time'] / 1000}",
-                task["nice"],
-                f"{task['waiting_time'] / 1000}",
-                f"{task['turnaround_time'] / 1000}",
-            ]
-        )
-    print(
-        "\n"
-        + tabulate(tasks_mat, headers=headers, tablefmt="fancy_grid", floatfmt=".3f")
-    )
-
-
-def findAvgTime(tasks: List[dict]):
-    """
-    Find average waiting and turnaround time.
-    """
-
-    waiting_times = []
-    total_wt = 0
-    total_tat = 0
-    num = len(tasks)
-
-    for task in tasks:
-        waiting_times.append(task["waiting_time"])
-        total_wt += task["waiting_time"]
-        total_tat += task["turnaround_time"]
-
-    print(f"\nAverage waiting time: {total_wt / (num * 1000): .3f} seconds")
-    print(f"Average turnaround time: {total_tat / (num * 1000): .3f} seconds")
-    print(
-        "Standard deviation in waiting time: "
-        f"{numpy.std(waiting_times) / 1000: .3f} seconds"
-    )
-
-
 def initTasks(tasks: List[dict]):
     for task in tasks:
         task["vruntime"] = 0
@@ -208,22 +149,22 @@ def initTasks(tasks: List[dict]):
         task["turnaround_time"] = 0
 
 def drawResult(xcood,processes):
-    plt.title('CFS Result')  # 折线图标题
-    plt.xlabel('time(ms)')  # x轴标题
-    plt.ylabel('CPU burst time')  # y轴标题
-    for i in processes[1]:
-        print(i)
-    # print('A:',processes[0],'\n')
-    # print('B:',processes[1],'\n')
-    # print('C:',processes[2],'\n')
-    
-    # plt.xticks([0,1000,2000,3000,4000,5000,6000,7000,8000,9000,10000])
+    plt.title('CFS Result')  # title of the image
+    plt.xlabel('CPU time (ms)')  # x axis title
+    plt.ylabel('Total burst time')  # y axis title
     
     for i in range(0,len(xcood)):
         plt.plot(xcood[i],processes[i],color=(random.randint(0,255)/255,random.randint(0,255)/255,random.randint(0,255)/255))
 
+    legendString = []
+    for i in range(0,len(xcood)):
+        processString = 'Process' + str(i+1)
+        legendString.append(processString)
+    
+    plt.legend(legendString)
+        
     plt.show()
-    #x [1,2] [,] [,]
+
 def printlog(tasks,min_task,timer,processRuntime,logger):
     taskIndex = tasks.index(min_task) #0 1 2
     log = str(timer)
@@ -235,7 +176,7 @@ def printlog(tasks,min_task,timer,processRuntime,logger):
 
     print(log)
     logger.debug(log)
-
+    
 def generateRandomProcess(N,MAX_ARRIVAL_TIME,MAX_BURST_TIME,MAX_NICE_VALUE):
     return random.randint(1, N * N),
     random.randint(0, MAX_ARRIVAL_TIME),
@@ -247,11 +188,10 @@ if __name__ == "__main__":
     logger = create_logger('test')
     MIN_VERSION = (3, 8)
     logging.basicConfig(level = logging.WARNING)
-    QUANTUM = 4  # sysctl_sched_latency 
     MAX_ARRIVAL_TIME = 20_000
     MAX_BURST_TIME = 50_000
     MAX_NICE_VALUE = 20
-
+    QUANTUM = int(input("Enter the TimeSlice value: "))
     N = int(input("Enter the number of tasks: "))
     TASKS = []
 
@@ -259,7 +199,7 @@ if __name__ == "__main__":
     print('(Input Times in milliseconds)')
     for _ in range(N):
         pid, arrivalTime, burseTime, nice = tuple(int(x) for x in input().split())
-        # pid, at, bt, nice = generateRandomProcess(N,MAX_ARRIVAL_TIME,MAX_BURST_TIME,MAX_NICE_VALUE)
+        # pid, at, bt, nice = generateRandomProcess(N,MAX_ARRIVAL_TIME,MAX_BURST_TIME,MAX_NICE_VALUE) #random generate process data
         
         TASKS.append(
             {
@@ -275,12 +215,8 @@ if __name__ == "__main__":
             }
         )
 
-    # Sort tasks by arrival time
-    TASKS_SORTED = SortedKeyList(TASKS, key=lambda task: task["arrival_time"])
 
-    # Schedule tasks according to CFS algorithm and print average times
+    TASKS_SORTED = SortedKeyList(TASKS, key=lambda task: task["arrival_time"])    # Sort tasks by arrival time
+
     initTasks(TASKS_SORTED)
     cfsSchedule(TASKS_SORTED, QUANTUM, logger)
-    displayTasks(TASKS)
-    findAvgTime(TASKS)
-
